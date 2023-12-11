4f9982b0bb9515378d2c0dc7ad65f2ef988c5fbb
fakamda <facundoelcardenal@gmail.com>
2023-12-10 21:10:14 -0300

    hash

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,25 @@
 import userModel from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
     const { email, password, username } = req.body
 
-    console.log(email, password, username)
+    try {
+        const passwordHash = await bcrypt.hash(password, 10)
 
-    res.send('registrando')
+        const newUser = new User({
+            username,
+            email,
+            password: passwordHash
+        })
+    
+        const userSaved = await newUser.save()
+        res.status(200).json({ userSaved })
+    
+    } catch (error) {
+        console.log(error)
+    }
+   
 }
 
 export const login = (req, res) => {
