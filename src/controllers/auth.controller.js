6a3bebafbb970bc4a3fa81611c5b96307bd6b932
fakamda <facundoelcardenal@gmail.com>
2023-12-10 21:37:42 -0300

    a

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,7 +28,7 @@ export const register = async (req, res) => {
             
     
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ message: error.mesagge })
     }
    
 }
