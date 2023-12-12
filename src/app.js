a68d5b2a80cff556d5441723d1284cf9d87309be
fakamda <facundoelcardenal@gmail.com>
2023-12-12 01:37:40 -0300

    tasks

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
 
 import AuthRoutes from './routes/auth.routes.js'
-import cookieParser from 'cookie-parser'
+import TaskRoutes from './routes/tasks.routes.js'
+
 
 const app =  express()
 
@@ -11,5 +13,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api', AuthRoutes)
+app.use('/api', TaskRoutes)
 
 export default app
