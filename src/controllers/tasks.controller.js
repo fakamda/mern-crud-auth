473ad38c9c53407d4342bcbd2ba889e7d516fe27
fakamda <facundoelcardenal@gmail.com>
2023-12-12 01:42:45 -0300

    crud tasks

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,6 @@
+
+export const getTasks = async (req, res) => {}
+export const getTaskById = async (req, res) => {}
+export const createTask = async (req, res) => {}
+export const deleteTask = async (req, res) => {}
+export const uptdateTask = async (req, res) => {}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
+import { createTask, deleteTask, getTaskById, getTasks, uptdateTask } from "../controllers/tasks.controller.js";
 
 const router = Router()
 
-router.get('/tasks', authRequired, (req, res) => res.send('tasks') )
+router.get('/tasks', authRequired, getTasks )
+router.get('/tasks/:id', authRequired, getTaskById )
+router.post('/tasks', authRequired, createTask )
+router.delete('/tasks/:id', authRequired, deleteTask )
+router.put('/tasks/:id', authRequired, uptdateTask )
 
 export default router
