e186c4df237aae25806555ff8201e213693e079e
fakamda <facundoelcardenal@gmail.com>
2023-12-12 01:53:51 -0300

    crud complete

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,6 +1,39 @@
+import taskModel from "../models/user.model.js"
 
-export const getTasks = async (req, res) => {}
-export const getTaskById = async (req, res) => {}
-export const createTask = async (req, res) => {}
-export const deleteTask = async (req, res) => {}
-export const uptdateTask = async (req, res) => {}
+
+export const getTasks = async (req, res) => {
+    const tasks = taskModel.find()
+    res.json(tasks)
+}
+
+export const getTaskById = async (req, res) => {
+    const task = await taskModel.findById(req.params.id)
+    if(!task) return res.status(404).json({ message: "not found" })
+    res.json(task)
+}
+
+export const createTask = async (req, res) => {
+    const { title, description, date } = req.body
+
+    const newTask = new taskModel({
+        title, 
+        description,
+        date
+    })
+
+    const savedTask = await newTask.save()
+    res.json(savedTask)
+}
+
+export const deleteTask = async (req, res) => {
+    const task = await taskModel.findByIdAndDelete(req.params.id)
+    if(!task) return res.status(404).json({ message: "not found" })
+    res.json(task)
+
+}
+
+export const uptdateTask = async (req, res) => {
+    const task = await taskModel.findByIdAndUpdate(req.params.id, req.body , { new : true })
+    if(!task) return res.status(404).json({ message: "not found" })
+    res.json(task)
+}
