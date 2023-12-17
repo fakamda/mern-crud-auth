38518e1fae01ac073fc0c66169cf7dea43dc8313
Facundo Dominguez <facundo.dominguez@intranet.telviso.com.ar>
2023-12-17 20:37:50 -0300

    protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext"
 
+import HomePage from "./pages/HomePage"
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
 import TasksPage from "./pages/TasksPage"
 import TaskFormPage from "./pages/TaskFormPage"
 import ProfilePage from "./pages/ProfilePage"
-import HomePage from "./pages/HomePage"
+
+import ProtectedRoute from "./ProtectedRoute"
+
 
 
 
@@ -18,10 +21,12 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/tasks' element={<TasksPage />} />
-          <Route path='/add-task' element={<TaskFormPage />} />
-          <Route path='/tasks/:id' element={<TaskFormPage/>} />
-          <Route path='/profile' element={<ProfilePage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path='/tasks' element={<TasksPage />} />
+            <Route path='/add-task' element={<TaskFormPage />} />
+            <Route path='/tasks/:id' element={<TaskFormPage/>} />
+            <Route path='/profile' element={<ProfilePage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
