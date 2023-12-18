dcb52159d65b877f111c60aee0593ccf41730414
fakamda <facundoelcardenal@gmail.com>
2023-12-18 13:22:05 -0300

    tasks context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import TaskFormPage from "./pages/TaskFormPage"
 import ProfilePage from "./pages/ProfilePage"
 
 import ProtectedRoute from "./ProtectedRoute"
+import { TaskProvider } from "./context/TasksContext"
 
 
 
@@ -16,19 +17,21 @@ import ProtectedRoute from "./ProtectedRoute"
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path='/tasks' element={<TasksPage />} />
-            <Route path='/add-task' element={<TaskFormPage />} />
-            <Route path='/tasks/:id' element={<TaskFormPage/>} />
-            <Route path='/profile' element={<ProfilePage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <TaskProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path='/tasks' element={<TasksPage />} />
+              <Route path='/add-task' element={<TaskFormPage />} />
+              <Route path='/tasks/:id' element={<TaskFormPage/>} />
+              <Route path='/profile' element={<ProfilePage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TaskProvider>
     </AuthProvider>
   )
 }
