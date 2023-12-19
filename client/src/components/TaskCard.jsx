d7686ccedf2ead5435a0f1429db66b71d4adc4d7
fakamda <facundoelcardenal@gmail.com>
2023-12-19 14:43:50 -0300

    delete task

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,11 +1,14 @@
+import { useTasks } from "../context/TasksContext"
 
 function TaskCard() {
+
+const { deleteTask } = useTasks()
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
        <header className="flex justify-between">
         <h3 className="text-2xl font-bold">{task.title}</h3>
         <div className="flex gap-x-2 items-center">
-            <button onClick={()=> {}}>delete</button>
+            <button onClick={()=> {deleteTask(task._id)}}>delete</button>
             <button>edit</button>
         </div>
        </header>
--- client/src/context/TasksContext.jsx
+++ client/src/context/TasksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { createTasksRequest, getTasksRequest } from "../api/tasks";
+import { createTasksRequest, getTasksRequest, deleteTasksRequest } from "../api/tasks";
 
 const TaskContext = createContext()
 
@@ -31,8 +31,19 @@ export function TaskProvider({ children }) {
         console.log(res)
     }
 
+    const deleteTask = async (id) => {
+        try{
+            const res = await deleteTasksRequest(id)
+            if(res.status === 204) setTasks(tasks.filter(task => task._id !== id))
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return(
-        <TaskContext.Provider value={{tasks, getTasks, createTask}}>
+        <TaskContext.Provider value={{tasks, getTasks, createTask, deleteTask}}>
             {children}
         </TaskContext.Provider>
     )
